efdbea371d31c39f3b2c5fc92ff978600215877b
Nikola Novak <nikolan97@live.com>
2019-11-02 21:19:38 +0100

    Adding new items to the data controller & UI

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
-//BUDGET CONTROLLER
+////////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////BUDGET CONTROLLER////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+
 var budgetController = (function() {
     
     var Expense = function(id, description, value) {
@@ -24,17 +27,53 @@ var budgetController = (function() {
         }
     };
     
+    return {
+        addItem: function (type, des, val) {
+            var newItem, ID;
+            
+            // ID = lastID + 1:
+            // Create new ID
+            if (data.allItems[type].length > 0) {
+                ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+            // Create new item based on 'inc' or 'exp' type
+            if (type === 'exp') {
+                newItem = new Expense(ID, des, val);
+            } else if (type === 'inc')  {
+                newItem = new Income(ID, des, val);
+            } else {
+                console.log('Unknown type of newItem.');
+            }
+            
+            // Push it into our data structure
+            data.allItems[type].push(newItem);
+            
+            //Returen the new element
+            return newItem;
+        },
+        
+        testing: function() {
+            console.log(data);
+        }
+    };
 })();
 
+////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////UI CONTROLLER//////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
 
-//UI CONTROLLER
 var UIController = (function() {
     
     var DOMstrings = {
         inputType: '.add__type',
         inputDescription: '.add__description',
         inputValue: '.add__value',
-        inputButton: '.add__btn'
+        inputButton: '.add__btn',
+        incomeContainer: '.income__list',
+        expenseContainer: '.expenses__list'
     };
     
     return {
@@ -46,15 +85,41 @@ var UIController = (function() {
             };
         },
         
+        addListItem: function(obj, type) {
+            var html, newHtml, element;
+            
+            // Create HTML string with placeholder text
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+                
+                html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type === 'exp') {
+                element = DOMstrings.expenseContainer;
+                
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
+            
+            // Replace the placeholder text with some actual data
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value);
+            
+            // Insert the HTML into the DOM
+            document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+            
+        },
+        
         getDOMstrings: function() {
             return DOMstrings;
         }
     };
     
 })();
-
-
-//GLOBAL APP CONTROLLER
+    
+////////////////////////////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////GLOBAL APP CONTROLLER//////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////
+    
 var controller = (function(budgetCtrl, UICtrl) {
     
     var setupEventListeners = function() {    
@@ -70,13 +135,16 @@ var controller = (function(budgetCtrl, UICtrl) {
     };
 
     var ctrlAddItem = function() {
+        var input, newItem;
         
         // 1. Get the field input data
-        var input = UICtrl.getInput();
+        input = UICtrl.getInput();
         
         // 2. Add the item to the budget controller 
+        newItem = budgetCtrl.addItem(input.type, input.description, input.value);
         
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
         
         // 4. Calculate the budget
         
